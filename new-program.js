fff60e30aec714dfe96b046245f9c8925b5f601a
este117 <54929311+este117@users.noreply.github.com>
2019-11-24 22:39:11 -0700

    Update new-program.js

--- new-program.js
+++ new-program.js
@@ -1,58 +1,37 @@
 led.enable(false)
-pins.analogWritePin(AnalogPin.P6, 0)
-pins.analogWritePin(AnalogPin.P8, 0)
-pins.analogWritePin(AnalogPin.P9, 0)
 
 class Lights {
-    
-    swirl() {
-    for (let x = 0; x <= 4; x++) {
-        led.plot(x, 0)
-        led.plot(0, 4 - x)
-        led.plot(4 - x, 4)
-        led.plot(4, x)
-        basic.pause(50)
-        led.unplot(x, 0)
-        led.unplot(4 - x, 4)
-        led.unplot(0, 4 - x)
-        led.unplot(4, x)
-        basic.pause(50)
+     fadeLight() {
+        for (let index = 0; index <= 3071; index++) {
+            pins.analogWritePin(AnalogPin.P3, index);
+            control.waitMicros(750) 
+        }
+        for (let index = 0; index <= 3071; index++) {
+            pins.analogWritePin(AnalogPin.P2, index)
+            control.waitMicros(750)
+        }
+        for (let index = 0; index <= 3071; index++) {
+            pins.analogWritePin(AnalogPin.P3, index);
+            control.waitMicros(750)
+        }
+        for (let index = 3071; index >= 0; index--) {
+            pins.analogWritePin(AnalogPin.P0, index);
+            control.waitMicros(750)
+        }
+        for (let index = 3071; index >= 0; index--) {
+            pins.analogWritePin(AnalogPin.P2, index)
+            control.waitMicros(750)
+        }
+        for (let index = 3071; index >= 0; index--) {
+            pins.analogWritePin(AnalogPin.P3, index);
+            control.waitMicros(750)
+        }
     }
-        
-        
-        
 }
-    
-    
-    
-    
-}
-
-
 basic.forever(function () {
-
-    //set pins that will be used to 0
-    pins.analogWritePin(AnalogPin.P0, 0);
-    pins.analogWritePin(AnalogPin.P2, 0);
     pins.analogWritePin(AnalogPin.P3, 0);
-    pins.analogWritePin(AnalogPin.P6, 0);
-
-    //flashes the three leds
-    let light = new LightUpLED;
-    light.flash();//runs flash function
-    light.eachLedLight();//runs eachLedLight function
-
+    pins.analogWritePin(AnalogPin.P4, 0);
+    pins.analogWritePin(AnalogPin.P10, 0);
+    let light = new Lights;
+    light.fadeLight(); 
 })
-
-
-basic.forever(function () {
-    
-    pins.analogWritePin(AnalogPin.P0, 0)
-    pins.analogWritePin(AnalogPin.P2, 0)
-    pins.analogWritePin(AnalogPin.P3, 0)
-    
-    let light = new LightUpLED;
-    light.flash();//runs flash function
-    light.eachLedLight();//runs eachLedLight function
-    
-}
